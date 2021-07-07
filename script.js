474be72ebb988aa9235fb45e043031be9994da74
code-reaper08 <v.i.s.h.w.a.08.11.2001@gmail.com>
2021-07-07 23:28:19 +0530

    Added basic working

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ function jmp() {
         player.classList.remove("animate");
     },500);
 }
+
+let checksts = setInterval(function () {
+    let playerTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"));
+    let obsleft = parseInt(window.getComputedStyle(obs).getPropertyValue("left"));
+    if (obsleft < 20 && obsleft>0 && playerTop>=130){
+        obs.style.animation = "none";
+        obs.style.display = "none";
+        alert("Looser")
+    }
+},10)

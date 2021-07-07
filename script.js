8cf64e28e4327bf52af03d49225d48766e49d455
code-reaper08 <v.i.s.h.w.a.08.11.2001@gmail.com>
2021-07-07 23:14:54 +0530

    Added conditions to control class multiplicity

--- script.js
+++ script.js
@@ -2,5 +2,10 @@ let player = document.getElementById('player');
 let obs =  document.getElementById('obs');
 
 function jmp() {
-    player.classList.add("animate");
+    if (player.classList != "animate"){
+        player.classList.add("animate");
+    }
+    setTimeout(function () {
+        player.classList.remove("animate");
+    },500);
 }
